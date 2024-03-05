3ad2b411bcec0384dc8faf104d50f88c71461a09
DanielDeanGithub <github-new@inboxrefresh.com>
2024-03-05 10:47:26 +0000

    Add empty event handler property with a click event on nextOne element

--- main.js
+++ main.js
@@ -33,7 +33,9 @@ nextThree.hidden = true;
 startOver.hidden= true;
 
 // Write anonymous event handler property and function for the first progress button
+nextOne.addEventListener('click', () => {
 
+});
 
 // Write anonymous event handler property and function for the second progress button
 
